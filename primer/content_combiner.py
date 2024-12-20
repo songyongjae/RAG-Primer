4b6c34d27ef23f95d580692a0631f6c7480ca0a0
SYJ <131941451+songyongjae@users.noreply.github.com>
2024-12-20 19:22:15 +0900

    Delete primer directory

--- primer/content_combiner.py
+++
@@ -1,8 +0,0 @@
-def combine_content(documents):
-    """
-    Combines the 'chunk' content from all documents into a single string.
-
-    :param documents: List of documents loaded from JSON files.
-    :return: Combined content as a single string.
-    """
-    return "\n\n".join(doc.get('chunk', '') for doc in documents)
